1febc51a71a4933ff66d2a7d873cd0a95c1286b1
ROBERTO GOMES DE CARVALHO NETO <roberto.neto005@cs.cruzeirodosul.edu.br>
2026-08-20 11:05:27 -0300

    exercicio 1 teste 1

--- src/validator.ts
+++ src/validator.ts
@@ -2,6 +2,14 @@ function validarChamado(titulo: string): boolean {
     if (!titulo || titulo.trim()==="") {
         throw new Error("O título do chamado não pode ser vazio.");
     }
+    // Novas regras
+  if (titulo.trim().length < 5) {
+    throw new Error("O título deve possuir no mínimo 5 caracteres.");
+  }
+
+  if (titulo.trim().length > 100) {
+    throw new Error("O título deve possuir no máximo 100 caracteres.");
+  }
     return true;
 }
 
